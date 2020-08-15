1eba5e259eaa6bcac23903eaa5799d7534f2ca6f
michaelchun <michael.chun03@gmail.com>
2020-08-15 16:36:48 -0400

    ws test

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -4,6 +4,35 @@ import TextField from "@material-ui/core/TextField";
 import "../css/landing.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      test: null,
+    };
+  }
+
+  ws = new WebSocket("ws://localhost:8000/");
+
+  componentDidMount() {
+    this.ws.onopen = () => {
+      // on connecting, do nothing but log it to the console
+      console.log("connected");
+    };
+
+    this.ws.onmessage = (evt) => {
+      // listen to data sent from the websocket server
+      const message = JSON.parse(evt.data);
+      this.setState({ dataFromServer: message });
+      console.log(message);
+    };
+
+    this.ws.onclose = () => {
+      console.log("disconnected");
+      // automatically try to reconnect on connection loss
+    };
+  }
+
   handleCreateGame = () => {
     // create game
     createGame();
