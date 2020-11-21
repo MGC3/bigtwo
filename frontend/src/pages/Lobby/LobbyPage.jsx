aa848e7954a3245c3ff6df990396f001d4fcd449
michaelchun <michael.chun03@gmail.com>
2020-11-20 23:03:31 -0500

    Barebones lobby page setup to accept WS room state and hopefully can display users

--- frontend/src/pages/Lobby/LobbyPage.jsx
+++ frontend/src/pages/Lobby/LobbyPage.jsx
@@ -5,18 +5,54 @@ export const LobbyPage = ({
   match: {
     params: { roomId },
   },
+  socket,
 }) => {
-  const [roomCode, setRoomCode] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [playerId, setPlayerId] = useState(null);
+  const [players, setPlayers] = useState([]);
 
   useEffect(() => {
-    roomId ? setRoomCode(roomId) : setRoomCode("error");
+    socket.send(
+      JSON.stringify({
+        type: "request_room_state",
+      })
+    );
   }, []);
+
+  useEffect(() => {
+    socket.onmessage = function (event) {
+      const message = JSON.parse(event.data);
+      const { type, data } = message;
+
+      switch (type) {
+        case "room_state":
+          setPlayerId(data.client_id);
+          setPlayers(data.players);
+          setIsLoading(false);
+          break;
+        default:
+          console.warn("received unknown WS type");
+      }
+    };
+  });
 
   return (
     <PageWrapper>
-      <div>
-        <h1>Room id is: {roomCode}</h1>
-      </div>
+      {isLoading ? (
+        "Loading"
+      ) : (
+        <div>
+          <h1>Room id is: {roomId}</h1>
+          <h1>Player id is: {playerId}</h1>
+          <h1>You are: {players[playerId]}</h1>
+          <h1>Players list:</h1>
+          <ul>
+            {players.map((name) => (
+              <li>{name}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </PageWrapper>
   );
 };
